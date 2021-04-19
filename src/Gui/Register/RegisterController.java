7f7e371d791c348811d5d45ed1ab80e399a45fb2
Ryaan <merghed.rayen@esprit.tn>
2021-04-19 13:37:29 +0200

    Updates

--- src/Gui/Register/RegisterController.java
+++ src/Gui/Register/RegisterController.java
@@ -5,9 +5,10 @@
  */
 package Gui.Register;
 
+import javafx.fxml.Initializable;
+
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 
 /**
  * FXML Controller class
@@ -21,7 +22,7 @@ public class RegisterController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
     }    
     
 }
